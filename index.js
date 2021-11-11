7b7edadec203baddba51020ecd6a43ec6fc1c384
Timofii <timoxafos@gmail.com>
2021-11-11 12:01:04 +0200

    some

--- index.js
+++ index.js
@@ -3,17 +3,38 @@ class Counter extends React.Component {
     super(props);
     this.state = { counter: 0 };
   }
+
+  icrement =() => {
+    this.setState({counter: this.state.counter + 1})
+  }
+  decrement=()=> {
+    this.setState({counter: this.state.counter > 0 ? this.state.counter - 1 : this.state.counter})
+  }
+  
   render() {
     const { counter } = this.state;
     return React.createElement(
       React.Fragment,
       null,
       React.createElement("h1", null, counter),
-      React.createElement("button", null, "+"),
-      React.createElement("button", null, "-")
+      React.createElement("button", {onClick: this.icrement}, "+"),
+      React.createElement("button", {onClick: this.decrement}, "-")
     );
   }
 }
 
 const reactElement = React.createElement(Counter);
 ReactDOM.render(reactElement, document.getElementById("root"));
+
+
+
+
+
+
+
+
+
+
+
+
+// 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
